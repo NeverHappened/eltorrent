ddf63355e4911581c6779da8680f2a3cac034c50
nhpd <dmitry.klpv@gmail.com>
2017-03-19 19:17:27 +0300

    Refactor constant

--- lib/eltorrent/torrent/model/torrent_pieces.ex
+++ lib/eltorrent/torrent/model/torrent_pieces.ex
@@ -1,15 +1,16 @@
 defmodule Eltorrent.TorrentPieces do
 
+  @sha1_bytes_per_piece 20
+
   def get_all_sha_pieces(torrent_file) do
     get_all_sha_pieces_simple(torrent_file.pieces)
   end
 
   def get_all_sha_pieces_simple(pieces) do # pieces - is a list of bytes
-    sha1_bytes_per_piece = 160 / 8 # every piece contains 20 bytes
-    piece_length = round Float.ceil(length(pieces) / sha1_bytes_per_piece) # how many pieces are there? length / 20
+    piece_length = round Float.ceil(length(pieces) / @sha1_bytes_per_piece) # how many pieces are there? length / 20
     piece_indexes = 0..(piece_length - 1)
 
-    Enum.map(piece_indexes, fn index -> get_sha_pieces(pieces, index, sha1_bytes_per_piece) end)
+    Enum.map(piece_indexes, fn index -> get_sha_pieces(pieces, index) end)
   end
 
   def get_all_sha_pieces_for_file(pieces_list, file_byte_start, file_byte_end, file_bytes_in_piece) do
@@ -18,10 +19,10 @@ defmodule Eltorrent.TorrentPieces do
     Enum.slice(pieces_list, start_index..end_index)
   end
 
-  def get_sha_pieces(pieces, piece_index, sha1_bytes_per_piece) do
+  def get_sha_pieces(pieces, piece_index) do
     # note: we use the fact that files are in the same order that pieces sha's concatenated
-    from_byte = round(piece_index * sha1_bytes_per_piece)
-    to_byte = round(Enum.min([from_byte + sha1_bytes_per_piece, length(pieces)]) - 1)
+    from_byte = round(piece_index * @sha1_bytes_per_piece)
+    to_byte = round(Enum.min([from_byte + @sha1_bytes_per_piece, length(pieces)]) - 1)
 
     Enum.slice(pieces, from_byte..to_byte)
   end
